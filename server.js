a78c70e390662966ed5ee5d4b6aad9e3259126a3
madihg <madihalim@gmail.com>
2025-07-11 12:11:57 -0400

    Fix Socket.IO server for Vercel deployment

--- server.js
+++ server.js
@@ -3,16 +3,23 @@ const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 let userCount = 0;
 
 app.use(express.static("public")); // Serve static files from 'public' directory
 
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+// Serve the word spreader at root
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
+// Socket.IO connection handling
 io.on("connection", (socket) => {
   console.log("A user connected");
 
@@ -29,3 +36,12 @@ io.on("connection", (socket) => {
     io.emit("userCount", userCount);
   });
 });
+
+// For Vercel serverless
+if (process.env.NODE_ENV !== 'production') {
+  server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
+}
+
+module.exports = app;
